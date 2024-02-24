ae2ec1777fc46dc53ac2a1b3b8a6c217787e2147
Sven <svenrisse0@gmail.com>
2024-02-24 10:00:57 +0100

    fix typo and add routes

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,15 @@ import express from "express";
 import config from "config";
 import { connect } from "./utils/connect";
 import { log } from "./utils/logger";
+import { routes } from "./routes";
 
 const app = express();
 
 const port = config.get<number>("port");
 
 app.listen(port, async () => {
-  log.info(`App is runnig at http://localhost:${port}`);
+  log.info(`App is running at http://localhost:${port}`);
   await connect();
+
+  routes({ app: app });
 });
