38a629866d825f8c98928d4265506914a46d01e7
Sven <svenrisse0@gmail.com>
2024-02-24 10:01:13 +0100

    add validation middleware

---
+++ src/middleware/validateResource.ts
@@ -0,0 +1,18 @@
+import { type Request, type Response, type NextFunction } from "express";
+import { ZodError, type AnyZodObject } from "zod";
+export const validate =
+  (schema: AnyZodObject) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+    } catch (e) {
+      if (e instanceof ZodError) {
+        return res.status(400).send(e.message);
+      }
+      return res.status(400).send(String(e));
+    }
+  };
